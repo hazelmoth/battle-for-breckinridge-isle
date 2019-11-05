0f45cb516a7f926d6f19a8456798c9b4b63d377f
Linus Waddell <linuswaddell@Linuss-MacBook-Pro.local>
2019-11-04 20:09:15 -0800

    set up basic neural network stuff

---
+++ Assets/Machine Learning Stuff/MathHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathHelper
+{
+	public static float WeightedSum(float[] inputs, float[] weights)
+	{
+		if (inputs.Length != weights.Length)
+		{
+			Debug.LogError("Number of weights doesn't match number of inputs!");
+			return 0;
+		}
+		float result = 0;
+		for (int i = 0; i < inputs.Length; i++)
+		{
+			result += inputs[i] * weights[i];
+		}
+
+		return result;
+	}
+
+    public static float HyperbolicTangent (float input)
+	{
+		//Debug.Log();
+		return (float)System.Math.Tanh(input);
+	}
+}
